9487505ad38309d07a7d0b45a3b7adeb44a47723
82surf <82surf@gmail.com>
2022-03-25 20:41:42 +0900

    feat: 🎸 [Frontend] Vue Frontend init
    - index 페이지 1차 구현

---
+++ frontend-vue/src/layout/MainNavbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <navbar
+    position="fixed"
+    type="success"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+    <template>
+      <router-link v-popover:popover1 class="navbar-brand" to="/"> Hand to Hand </router-link>
+    </template>
+    <template slot="navbar-menu">
+      <li class="nav-item pr-2">
+        <a
+          class="nav-link"
+          href="https://www.creative-tim.com/product/vue-now-ui-kit"
+          target="_blank"
+        >
+          <i class="now-ui-icons media-1_album"></i>
+          <p>NFT 갤러리</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link btn btn-success btn-round"
+          href="https://www.creative-tim.com/product/vue-now-ui-kit-pro"
+          target="_blank"
+        >
+          <i class="now-ui-icons users_single-02 pr-1"></i>
+          <p>지갑 연결하기</p>
+        </a>
+      </li>
+    </template>
+  </navbar>
+</template>
+
+<script>
+import { DropDown, Navbar, NavLink } from "@/components";
+import { Popover } from "element-ui";
+export default {
+  name: "main-navbar",
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number,
+  },
+  components: {
+    // DropDown,
+    Navbar,
+    // NavLink,
+    [Popover.name]: Popover,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend-vue/src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="page-header clear-filter" filter-color="green">
+      <parallax class="page-header-image" style="background-image: url('img/ukrain1.jpg')">
+      </parallax>
+      <div class="container d-flex align-items-center justify-content-between flex-wrap">
+        <div class="text-left">
+          <h1>우크라이나에 기부하세요</h1>
+          <h3>AI가 그린 우크라이나 풍경화 NFT를 드립니다</h3>
+        </div>
+        <donate-form></donate-form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Parallax } from "@/components";
+import DonateForm from "./components/DonateForm.vue";
+
+export default {
+  name: "index",
+  bodyClass: "index-page",
+  components: {
+    Parallax,
+    DonateForm,
+  },
+};
+</script>
+<style></style>
