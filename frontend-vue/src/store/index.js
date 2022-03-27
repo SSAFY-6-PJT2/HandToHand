26702f542587a4cf80ede4a2be6804bb05a64705
82surf <82surf@gmail.com>
2022-03-27 23:15:14 +0900

    feat: 🎸 [Frontend] Vuex 추가

--- frontend-vue/src/store/index.js
+++ frontend-vue/src/store/index.js
@@ -1,15 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    priveKey: null,
+    address: null,
+  },
+  getters: {
+    isLogin: (state) => {
+      return state.address !== null;
+    },
   },
   mutations: {
+    SET_PRIV_KEY: (state, privKey) => {
+      state.priveKey = privKey;
+    },
+    SET_ADDRESS: (state, address) => {
+      state.address = address;
+    },
   },
   actions: {
+    vuexSetPrivKey({ commit }, privKey) {
+      commit('SET_PRIV_KEY', privKey);
+    },
+    vuexSetAddress({ commit }, address) {
+      commit('SET_ADDRESS', address);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
